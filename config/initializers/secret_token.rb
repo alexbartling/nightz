f547bc2b62d509e173a51318a30210313882afe1
abart711 <bartlingalex@gmail.com>
2013-04-09 11:36:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nightz::Application.config.secret_token = 'b90998e812b66fe9149e0c99c791feb94b08ffea3c19867629c617982c230590fc4988aafbdc6bb447f76de1611127a1fe41564d46a8c8a3a4f2d5dc3eee2729'
